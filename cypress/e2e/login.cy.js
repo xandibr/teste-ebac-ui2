2537fbd8ea7d9cf5edbc825d82abba191f83f498
Xandibr <xandibr@yahoo.com.br>
2023-07-21 19:53:17 -0300

    tentativa de usar o cypress config

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,39 +1,43 @@
 /// <reference types="cypress"/>
 
-context('Funcionalidade Login', () =>{
+context('Funcionalidade Login', () => {
+    http://lojaebac.ebaconline.art.br/
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+       
+
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
         cy.screenshot()
     });
 
-    it('Deve fazer login com sucesso', () =>{
+    it('Deve fazer login com sucesso', () => {
+
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.page-title').should('contain' , 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should ('contain', 'Olá, Aluno')
+        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, Aluno')
 
     })
-    
-    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () =>{
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
         cy.get('#username').type('lebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should ('contain' , 'Endereço de e-mail desconhecido')
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido')
 
     })
 
-    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () =>{
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
-        cy.get('.woocommerce-error > li').should ('contain' , 'Erro')
+        cy.get('.woocommerce-error > li').should('contain', 'Erro')
     })
 })
