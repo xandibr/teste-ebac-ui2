d09221a3a0e69c85187ad5ce20504159984ec825
Xandibr <xandibr@yahoo.com.br>
2023-07-21 21:56:39 -0300

    endereço

---
+++ cypress/e2e/endereco.cy.js
@@ -0,0 +1,12 @@
+describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
+    beforeEach(() => {
+        cy.visit('minha-conta')
+        cy.fixture('perfil').then(dados =>{
+            cy.login(dados.usuario, dados.senha)
+        })
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
+
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,10 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (usuario, senha) => {
+    cy.get('#username').type(usuario)
+    cy.get('#password').type(senha)
+    cy.get('.woocommerce-form > .button').click()
+
+})
